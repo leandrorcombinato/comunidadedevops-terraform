a9ea00b31dff9552458ae872fc425a143962f2b4
Leandro Combinato <leandro.combinato@galleriabank.com.br>
2025-08-16 08:42:33 -0300

    Terraform Recursos

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "cidr_block" {
+  type        = string
+  description = "The CIDR block for the VPC"
+}
+
+variable "project_name" {
+  type        = string
+  description = "Project name for tagging resources"
+}
